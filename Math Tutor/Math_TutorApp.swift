dde5a50e50c5d80a9e2ce041dc0a5d1fc2a058bf
ThaizRodarte <thaizrodarte08@gmail.com>
2026-02-03 11:34:47 -0800

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by RODARTE, THAIZ on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
